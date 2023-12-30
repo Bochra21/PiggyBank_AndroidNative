614668c6dd4816737603fb5cdd34b4cea4d03fef
bochr <bochra.isitcom@gmail.com>
2023-12-30 14:47:08 +0100

    second commit

--- app/src/main/java/com/example/piggybankapp/AddExpenseFragment.java
+++ app/src/main/java/com/example/piggybankapp/AddExpenseFragment.java
@@ -3,45 +3,42 @@ package com.example.piggybankapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AddExpenseFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AddExpenseFragment extends Fragment {
+public class AddExpenseFragment extends Fragment
+{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText expenseName;
+    EditText amount;
 
-    public AddExpenseFragment() {
-        // Required empty public constructor
+    ImageView addBtn, cancelBtn;
+
+
+    public AddExpenseFragment()
+    {
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddExpenseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AddExpenseFragment newInstance(String param1, String param2) {
         AddExpenseFragment fragment = new AddExpenseFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +46,99 @@ public class AddExpenseFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_expense, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_expense, container, false);
+
+        expenseName = view.findViewById(R.id.expense_name);
+        amount = view.findViewById(R.id.amount);
+        addBtn = view.findViewById(R.id.add_btn2);
+        cancelBtn = view.findViewById(R.id.cancel_btn2);
+
+
+
+        if (TextUtils.isEmpty(expenseName.getText().toString()) && TextUtils.isEmpty(amount.getText().toString())) {
+
+
+            addBtn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+
+                    String expenseName2 = expenseName.getText().toString();
+                    String amount2 = amount.getText().toString();
+
+                    // Get the existing instance of ExpensesFragment
+                    ExpensesFragment expensesFragment = (ExpensesFragment) requireActivity()
+                            .getSupportFragmentManager().findFragmentByTag("expenses_fragment");
+
+                    if (expensesFragment != null)
+                    {
+                        // Call the updateData method of ExpensesFragment
+                        expensesFragment.updateData(expenseName2, amount2);
+                    }
+
+                    // Navigate back to ExpensesFragment if needed
+
+                    try {
+                        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                        fragmentManager.popBackStack();  // Remove AddExpenseFragment from the back stack
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    /*
+                    Bundle bundle = new Bundle();
+                    bundle.putString("name", expenseName2);
+                    bundle.putString("amount", amount2);
+                    //
+                    ExpensesFragment expensesFragment = new ExpensesFragment();
+                    expensesFragment.setArguments(bundle);
+
+                    try {
+                        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, expensesFragment, "expenses_fragment");
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        // Handle the exception or log the details
+                        System.out.println(" fragment ex");
+                    }
+
+
+                    */
+
+
+                }
+            });
+
+
+
+
+        } else {
+           System.out.println(" one of the edit texts is null");
+            Toast.makeText(getContext(), "Your message here", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+
+
+
+
+
+
+        return view;
     }
+
+
 }
--- app/src/main/java/com/example/piggybankapp/ExpensesFragment.java
+++ app/src/main/java/com/example/piggybankapp/ExpensesFragment.java
@@ -1,7 +1,10 @@
 package com.example.piggybankapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,9 +12,16 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,15 +31,48 @@ import android.widget.TextView;
 public class ExpensesFragment extends Fragment
 {
 
+    TextView t1, t2;
+
 
 
     TextView date ;
     ImageView addExpense;
 
     TextView year;
+    ListView listView ;
+
+    private List<Map<String, String>> expensesList = new ArrayList<>();
+    private MyAdapter adapter; // Replace YourAdapter with your actual adapter class
+
+
 
     public ExpensesFragment() {
 
+    }
+
+
+    public void updateData(String name, String amount) {
+        // Create a new expense map
+        Map<String, String> newExpense = new HashMap<>();
+        newExpense.put("name", name);
+        newExpense.put("amount", amount);
+
+        // Add the new expense to your list
+        expensesList.add(newExpense);
+
+        // Update your adapter if you are using one
+        if (adapter != null) {
+            adapter.notifyDataSetChanged();
+        }
+
+        // Find the existing instance of ExpensesFragment using the consistent tag
+        ExpensesFragment expensesFragment = (ExpensesFragment) requireActivity()
+                .getSupportFragmentManager().findFragmentByTag("expenses_fragment");
+
+        if (expensesFragment != null) {
+            // Call the updateData method of ExpensesFragment
+            expensesFragment.updateData(name, amount);
+        }
     }
 
 
@@ -43,7 +86,8 @@ public class ExpensesFragment extends Fragment
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
 
@@ -52,14 +96,33 @@ public class ExpensesFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
+
+
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_expenses, container, false);
         date= view.findViewById(R.id.date);
         year = view.findViewById(R.id.year);
+
+        // for test
+
+        String[] expenseNameArray = {"Expense1", "Expense2", "Expense3"};
+        String[] expenseAmountArray = {"$10", "$20", "$30"};
+
+        // Instantiate the adapter
+        adapter = new MyAdapter(getContext(), expenseNameArray, expenseAmountArray);
+
+        // Find the ListView by its ID and set the adapter
+        ListView listView = view.findViewById(R.id.expenses_list); // Make sure the ID matches the one in your layout
+        listView.setAdapter(adapter);
+
+
         // Retrieve data from arguments
         Bundle bundle = getArguments();
-        if (bundle != null) {
+        if (bundle != null)
+        {
             String month = bundle.getString("selected_month", "");
             String selectedYear = bundle.getString("year", "");
 
@@ -68,16 +131,28 @@ public class ExpensesFragment extends Fragment
         }
 
 
+
+
+        t1 = view.findViewById(R.id.textView);
+        t2 =  view.findViewById(R.id.textView3);
+
+
+       
+
+
+
+
         addExpense = view.findViewById(R.id.add_btn);
 
-        addExpense.setOnClickListener(new View.OnClickListener() {
+        addExpense.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view)
             {
                 try {
                     FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.fragment_container, new AddExpenseFragment());
+                    fragmentTransaction.replace(R.id.fragment_container, new AddExpenseFragment(),"add_expense_fragment");
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
                 } catch (Exception e) {
@@ -90,4 +165,40 @@ public class ExpensesFragment extends Fragment
         return view;
 
     }
+
+
+    // MyAdapter for list view
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String expenseName[];
+        String expenseAmount[];
+
+        MyAdapter(Context c, String expenseName[], String expenseAmount[]) {
+            super(c, R.layout.row, R.id.textView1, expenseName); // Use expenseName as the data source
+            this.context = c;
+            this.expenseName = expenseName;
+            this.expenseAmount = expenseAmount;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+            View row = layoutInflater.inflate(R.layout.row2, parent, false);
+
+            TextView expenseNameTextView = row.findViewById(R.id.textView1);
+            TextView expenseAmountTextView = row.findViewById(R.id.textView2);
+
+
+            expenseNameTextView.setText(expenseName[position]);
+            expenseAmountTextView.setText(expenseAmount[position]);
+
+            return row;
+        }
+    }
+
+
+
 }
